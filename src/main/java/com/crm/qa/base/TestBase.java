694e503d227a933f68e6ed657d15a2020e0f6410
Hugo Quezada <hugh.quezada8@gmail.com>
2018-11-03 17:32:38 -0600

    su

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -26,7 +26,8 @@ public class TestBase {
 		
 		prop = new Properties();
 		try {
-			FileInputStream fis = new FileInputStream("CRM/src/main/java/com/crm/qa/config/config.properties");
+			FileInputStream fis = new FileInputStream("C:\\Users\\zesh_\\CRM\\"
+					+ "src\\main\\java\\com\\crm\\qa\\config\\config.properties");
 			prop.load(fis);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
